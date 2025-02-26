47097071a90343e2f7a61d02357e32181b904a54
adam-tutor <80133045+adam-tutor@users.noreply.github.com>
2025-02-26 14:26:26 -0600

    Add files via upload

---
+++ lab_debug/sketchify.cpp
@@ -0,0 +1,75 @@
+/**
+ * @file sketchify.cpp
+ * Implementation of the sketchify function.
+ */
+#pragma GCC diagnostic ignored "-Wuninitialized"
+#include <cstdlib>
+#include <cmath>
+
+#include "cs225/PNG.h"
+#include "cs225/HSLAPixel.h"
+using namespace cs225;
+
+/**
+ * Creates a new output image.
+
+ * @param w width of the image
+ * @param h height of the image
+
+ * @return a pointer to the newly-created image
+ */
+PNG* setupOutput(unsigned w, unsigned h) {
+    PNG* image = new PNG(w, h);
+    return image;
+}
+
+/**
+ * Used to customize the color of the sketchified image.
+
+ * @return a pointer to the color to use when sketchifying
+ */
+HSLAPixel* myFavoriteColor() {
+    return new HSLAPixel(233, 0.8, 0.5); //we have to use the new operation instead of setting up a variable
+}
+
+void sketchify(std::string inputFile, std::string outputFile) {
+    // Load in.png
+    PNG* original = new PNG();
+
+    original->readFromFile(inputFile);
+    unsigned width = original->width();
+    unsigned height = original->height();
+
+    // Create out.png
+    PNG* output = nullptr; //we have to define output first by setting it to a nullptr
+    output = setupOutput(width, height);
+
+    // Load our favorite color to color the outline
+    HSLAPixel* myPixel = myFavoriteColor();
+
+    // Go over the whole image, and if a pixel differs from that to its upper
+    // left, color it my favorite color in the output
+    for (unsigned y = 1; 0 < y&&y < height; y++) { //without the y&&y, it would create an infinite error list. I kept running into it and it was annoying to have to set up the terminal again
+        for (unsigned x = 1; 0 < x&&x < width; x++) {
+            // Calculate the pixel difference
+            HSLAPixel & prev = original->getPixel(x - 1, y - 1);
+            HSLAPixel & curr = original->getPixel(x, y);
+            double diff = std::fabs(curr.h - prev.h);
+
+            // If the pixel is an edge pixel,
+            // color the output pixel with my favorite color
+            HSLAPixel* currOutPixel = &(output->getPixel(x, y));
+            if (diff > 20) {
+                *currOutPixel = *myPixel; //both of these should be dereferenced
+            }
+        }
+    }
+
+    // Save the output file
+    output->writeToFile(outputFile);
+
+    // Clean up memory
+    delete myPixel;
+    delete output;
+    delete original;
+}

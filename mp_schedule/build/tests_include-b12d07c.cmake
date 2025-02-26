428b5d4670314bbacc9bc2c28b189ff38f686d85
adam-tutor <80133045+adam-tutor@users.noreply.github.com>
2025-02-26 15:12:52 -0600

    Add files via upload

---
+++ mp_schedule/build/tests_include-b12d07c.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "/home/tutor2/cs225git/mp_schedule/build/tests_tests-b12d07c.cmake")
+  include("/home/tutor2/cs225git/mp_schedule/build/tests_tests-b12d07c.cmake")
+else()
+  add_test(tests_NOT_BUILT-b12d07c tests_NOT_BUILT-b12d07c)
+endif()
